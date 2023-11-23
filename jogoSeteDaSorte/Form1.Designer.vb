02519adfbf7921fc6aad6dacb077a5882f8c2456
diojr <diojr@DESKTOP-PV492N7>
2023-11-23 10:12:07 -0300

    Adicionar arquivos de projeto.

---
+++ jogoSeteDaSorte/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.num1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.num2 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.num3 = New System.Windows.Forms.Label()
+        Me.BtnPlay = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Green
+        Me.Label1.Location = New System.Drawing.Point(277, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Lucky Seven"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.num1)
+        Me.Panel1.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(173, 104)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(100, 100)
+        Me.Panel1.TabIndex = 1
+        '
+        'num1
+        '
+        Me.num1.AutoSize = True
+        Me.num1.Location = New System.Drawing.Point(27, 16)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(58, 67)
+        Me.num1.TabIndex = 0
+        Me.num1.Text = "0"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.num2)
+        Me.Panel2.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel2.Location = New System.Drawing.Point(336, 104)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(100, 100)
+        Me.Panel2.TabIndex = 2
+        '
+        'num2
+        '
+        Me.num2.AutoSize = True
+        Me.num2.Location = New System.Drawing.Point(19, 16)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(58, 67)
+        Me.num2.TabIndex = 0
+        Me.num2.Text = "0"
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Controls.Add(Me.num3)
+        Me.Panel3.Font = New System.Drawing.Font("Segoe UI", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel3.Location = New System.Drawing.Point(493, 104)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(100, 100)
+        Me.Panel3.TabIndex = 3
+        '
+        'num3
+        '
+        Me.num3.AutoSize = True
+        Me.num3.Location = New System.Drawing.Point(16, 16)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(58, 67)
+        Me.num3.TabIndex = 0
+        Me.num3.Text = "0"
+        '
+        'BtnPlay
+        '
+        Me.BtnPlay.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnPlay.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnPlay.Location = New System.Drawing.Point(64, 320)
+        Me.BtnPlay.Name = "BtnPlay"
+        Me.BtnPlay.Size = New System.Drawing.Size(100, 39)
+        Me.BtnPlay.TabIndex = 4
+        Me.BtnPlay.Text = "Play!"
+        Me.BtnPlay.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnExit.ForeColor = System.Drawing.Color.Red
+        Me.BtnExit.Location = New System.Drawing.Point(679, 463)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(59, 28)
+        Me.BtnExit.TabIndex = 5
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(271, 256)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(223, 192)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 510)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnPlay)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Lucky Game 7"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents num1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents num2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents num3 As Label
+    Friend WithEvents BtnPlay As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ jogoSeteDaSorte/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles num3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnPlay.Click
+        PictureBox1.Visible = False
+        Randomize()
+        num1.Text = CStr(Int(Rnd() * 10))
+        num2.Text = CStr(Int(Rnd() * 10))
+        num3.Text = CStr(Int(Rnd() * 10))
+        'This code is to test 3 numbers seven
+        'num1.Text = 7
+        'num2.Text = 7
+        'num3.Text = 7
+        'if all numbers are seven'
+        If (num1.Text = "7") And (num2.Text = "7") And (num3.Text = "7") Then
+            PictureBox1.Image = Image.FromFile("C:\angeles\jogoSeteDaSorteSln\winMax.png")
+            PictureBox1.Visible = True
+            Beep()
+        ElseIf (num1.Text = "7") Or (num2.Text = "7") Or (num3.Text = "7") Then
+            PictureBox1.Image = Image.FromFile("C:\angeles\jogoSeteDaSorteSln\win.png")
+            PictureBox1.Visible = True
+            Beep()
+        Else
+            PictureBox1.Image = Image.FromFile("C:\angeles\jogoSeteDaSorteSln\tryAgain.png")
+            PictureBox1.Visible = True
+            Beep()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        End
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub num1_Click(sender As Object, e As EventArgs) Handles num1.Click
+
+    End Sub
+End Class
